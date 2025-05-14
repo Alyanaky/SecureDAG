829de44112e3af31f2c8201a22065682486a4255
Alyanaky <007izosimov@gmail.com>
2025-05-14 09:41:40 +0500

    Update main.go

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/Alyanaky/SecureDAG/internal/crypto"
 	"github.com/Alyanaky/SecureDAG/internal/p2p"
 	"github.com/Alyanaky/SecureDAG/internal/storage"
 	"github.com/libp2p/go-libp2p/core/network"
@@ -19,7 +20,6 @@ import (
 func main() {
 	ctx := context.Background()
 	
-	// Инициализация хранилища
 	storePath := filepath.Join(".", "storage", fmt.Sprintf("node-%d", time.Now().UnixNano()))
 	storage, err := storage.NewBadgerStore(storePath)
 	if err != nil {
@@ -27,14 +27,12 @@ func main() {
 	}
 	defer storage.Close()
 
-	// Инициализация P2P
 	kadDHT, node, err := p2p.NewDHT(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer kadDHT.Close()
 
-	// Обработчик входящих соединений
 	node.SetStreamHandler("/secure-dag/1.0", func(s network.Stream) {
 		defer s.Close()
 		buf := make([]byte, 1024)
@@ -50,7 +48,6 @@ func main() {
 		}
 	})
 
-	// CLI команды
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "put":
@@ -72,6 +69,27 @@ func main() {
 			hash := os.Args[2]
 			data, _ := p2p.GetFromDHT(ctx, kadDHT, hash)
 			fmt.Printf("Retrieved data: %s\n", string(data))
+			
+		case "keys":
+			publicKeyPEM := crypto.PublicKeyToBytes(storage.PublicKey())
+			fmt.Printf("Public Key:\n%s\n", publicKeyPEM)
+			
+		case "put-encrypted":
+			if len(os.Args) < 4 {
+				log.Fatal("Usage: put-encrypted <file> <public-key.pem>")
+			}
+			
+			pubKeyBytes, _ := os.ReadFile(os.Args[3])
+			recipientPubKey, _ := crypto.BytesToPublicKey(pubKeyBytes)
+			
+			content, _ := os.ReadFile(os.Args[2])
+			hashes, _ := storage.SplitAndStore(bytes.NewReader(content), 256)
+			
+			for _, hash := range hashes {
+				data, _ := storage.GetBlock(hash)
+				p2p.ReplicateBlock(ctx, kadDHT, hash, data, 3)
+			}
+			fmt.Printf("Encrypted hashes: %v\n", hashes)
 		}
 	}
 
