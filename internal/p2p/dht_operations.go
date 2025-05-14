98999aa11ef8ceeec780bb929c2e6a66d58da7f5
Alyanaky <007izosimov@gmail.com>
2025-05-14 09:34:47 +0500

    Update dht_operations.go

--- internal/p2p/dht_operations.go
+++ internal/p2p/dht_operations.go
@@ -20,6 +20,38 @@ func PutToDHT(ctx context.Context, dht *dht.IpfsDHT, key string, value []byte) e
 	return dht.PutValue(ctx, "/secure-dag/"+key, value)
 }
 
+func ReplicateBlock(ctx context.Context, dht *dht.IpfsDHT, key string, value []byte, replicas int) error {
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(replicas)*putTimeout)
+	defer cancel()
+
+	providers := make([]peer.ID, 0, replicas)
+	
+	// Находим N ближайших узлов
+	peers, err := dht.FindPeers(ctx, key)
+	if err != nil {
+		return err
+	}
+	
+	for p := range peers {
+		if len(providers) >= replicas {
+			break
+		}
+		if p.ID != dht.Host().ID() { // Исключаем себя
+			providers = append(providers, p.ID)
+		}
+	}
+	
+	// Сохраняем на выбранные узлы
+	for _, pid := range providers {
+		err := dht.PutValue(ctx, "/secure-dag/"+key, value, dht.Quorum(1))
+		if err != nil {
+			log.Printf("Failed to replicate to %s: %v", pid, err)
+		}
+	}
+	
+	return nil
+}
+
 func GetFromDHT(ctx context.Context, dht *dht.IpfsDHT, key string) ([]byte, error) {
 	ctx, cancel := context.WithTimeout(ctx, getTimeout)
 	defer cancel()
