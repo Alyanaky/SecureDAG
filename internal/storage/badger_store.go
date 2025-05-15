7e86f9793aef8a1a9fdf035f22e131df048835ba
Alyanaky <007izosimov@gmail.com>
2025-05-15 15:25:21 +0500

    Update badger_store.go

--- internal/storage/badger_store.go
+++ internal/storage/badger_store.go
@@ -1,100 +1,100 @@
-// Добавить в структуру
-type BadgerStore struct {
-	db        *badger.DB
-	publicKey  *rsa.PublicKey
-	privateKey *rsa.PrivateKey
+package storage
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/Alyanaky/SecureDAG/internal/crypto"
+	"github.com/dgraph-io/badger/v4"
+)
+
+type BlockMetadata struct {
+	References int       `json:"refs"`
+	CreatedAt  time.Time `json:"created"`
+	Size       int       `json:"size"`
 }
 
-// Обновить конструктор
+type BadgerStore struct {
+	db         *badger.DB
+	publicKey  *rsa.PublicKey
+	privateKey *rsa.PrivateKey
+	crdt       *CRDTStore
+}
+
 func NewBadgerStore(path string) (*BadgerStore, error) {
-	// ... существующий код ...
-	
-	// Генерируем ключи при инициализации
+	opts := badger.DefaultOptions(path)
+	opts.Logger = nil
+	db, err := badger.Open(opts)
+	if err != nil {
+		return nil, err
+	}
+
 	privKey, pubKey, err := crypto.GenerateRSAKeys()
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return &BadgerStore{
 		db:         db,
 		publicKey:  pubKey,
 		privateKey: privKey,
+		crdt:       &CRDTStore{values: make(map[string][]CRDTValue)},
 	}, nil
 }
 
-// Обновить методы работы с данными
 func (s *BadgerStore) PutBlock(data []byte) (string, error) {
-	// Генерируем случайный AES-ключ для каждого блока
+	contentHash := sha256.Sum256(data)
+	existing, _ := s.GetBlock(fmt.Sprintf("%x", contentHash))
+	if existing != nil {
+		return fmt.Sprintf("%x", contentHash), nil
+	}
+
 	aesKey := make([]byte, 32)
 	if _, err := rand.Read(aesKey); err != nil {
 		return "", err
 	}
 
-	// Шифруем данные
 	encryptedData, err := crypto.EncryptData(data, aesKey)
 	if err != nil {
 		return "", err
 	}
 
-	// Шифруем AES-ключ
 	encryptedKey, err := crypto.EncryptAESKey(s.publicKey, aesKey)
 	if err != nil {
 		return "", err
 	}
 
-	// Сохраняем в BadgerDB
-	hash := sha256.Sum256(encryptedData)
-	key := fmt.Sprintf("%x", hash)
-	
+	hash := fmt.Sprintf("%x", sha256.Sum256(encryptedData))
 	err = s.db.Update(func(txn *badger.Txn) error {
-		if err := txn.Set([]byte(key), encryptedData); err != nil {
+		if err := txn.Set([]byte(hash), encryptedData); err != nil {
 			return err
 		}
-		return txn.Set([]byte("key_"+key), encryptedKey)
+		return txn.Set([]byte("key_"+hash), encryptedKey)
 	})
-	
-	return key, err
+
+	s.updateMetadata(hash, 1)
+	return hash, err
 }
 
-func (s *BadgerStore) GetBlock(hash string) ([]byte, error) {
-	var encryptedData, encryptedKey []byte
-	
-	// Получаем зашифрованные данные
-	err := s.db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte(hash))
-		if err != nil {
-			return err
+func (s *BadgerStore) updateMetadata(hash string, delta int) error {
+	return s.db.Update(func(txn *badger.Txn) error {
+		metaKey := "meta_" + hash
+		var meta BlockMetadata
+		
+		item, err := txn.Get([]byte(metaKey))
+		if err == nil {
+			val, _ := item.ValueCopy(nil)
+			json.Unmarshal(val, &meta)
 		}
-		return item.Value(func(val []byte) error {
-			encryptedData = append([]byte{}, val...)
-			return nil
-		})
+		
+		meta.References += delta
+		meta.CreatedAt = time.Now()
+		metaBytes, _ := json.Marshal(meta)
+		return txn.Set([]byte(metaKey), metaBytes)
 	})
-	if err != nil {
-		return nil, err
-	}
-
-	// Получаем зашифрованный ключ
-	err = s.db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte("key_"+hash))
-		if err != nil {
-			return err
-		}
-		return item.Value(func(val []byte) error {
-			encryptedKey = append([]byte{}, val...)
-			return nil
-		})
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	// Расшифровываем AES-ключ
-	aesKey, err := crypto.DecryptAESKey(s.privateKey, encryptedKey)
-	if err != nil {
-		return nil, err
-	}
-
-	// Расшифровываем данные
-	return crypto.DecryptData(encryptedData, aesKey)
 }
