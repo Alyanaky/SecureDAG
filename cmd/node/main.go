e064e89290994d03fe8c1c34136f69940431da3e
Alyanaky <007izosimov@gmail.com>
2025-05-15 15:17:46 +0500

    Update main.go

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -11,6 +12,7 @@ import (
 	"time"
 
 	"github.com/Alyanaky/SecureDAG/internal/crypto"
+	"github.com/Alyanaky/SecureDAG/internal/dag"
 	"github.com/Alyanaky/SecureDAG/internal/p2p"
 	"github.com/Alyanaky/SecureDAG/internal/storage"
 	"github.com/libp2p/go-libp2p/core/network"
@@ -39,57 +41,67 @@ func main() {
 		n, _ := s.Read(buf)
 		msg := string(buf[:n])
 		
-		if strings.HasPrefix(msg, "GET:") {
+		switch {
+		case strings.HasPrefix(msg, "GET:"):
 			hash := strings.TrimPrefix(msg, "GET:")
 			data, err := storage.GetBlock(hash)
 			if err == nil {
 				s.Write(data)
 			}
+		case strings.HasPrefix(msg, "DAG:"):
+			hash := strings.TrimPrefix(msg, "DAG:")
+			data, _ := storage.GetBlock(hash)
+			s.Write(data)
 		}
 	})
 
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "put":
-			content := []byte("Persistent SecureDAG data!")
+			content := []byte("SecureDAG data")
 			hashes, _ := storage.SplitAndStore(bytes.NewReader(content), 256)
 			fmt.Printf("Stored hashes: %v\n", hashes)
-			
-			for _, hash := range hashes {
-				if data, err := storage.GetBlock(hash); err == nil {
-					p2p.ReplicateBlock(ctx, kadDHT, hash, data, 3)
-					fmt.Printf("Published hash %s to DHT\n", hash)
-				}
-			}
-			
-		case "get":
-			if len(os.Args) < 3 {
-				log.Fatal("Usage: get <hash>")
-			}
-			hash := os.Args[2]
-			data, _ := p2p.GetFromDHT(ctx, kadDHT, hash)
-			fmt.Printf("Retrieved data: %s\n", string(data))
-			
-		case "keys":
-			publicKeyPEM := crypto.PublicKeyToBytes(storage.PublicKey())
-			fmt.Printf("Public Key:\n%s\n", publicKeyPEM)
-			
-		case "put-encrypted":
-			if len(os.Args) < 4 {
-				log.Fatal("Usage: put-encrypted <file> <public-key.pem>")
-			}
-			
-			pubKeyBytes, _ := os.ReadFile(os.Args[3])
-			recipientPubKey, _ := crypto.BytesToPublicKey(pubKeyBytes)
-			
-			content, _ := os.ReadFile(os.Args[2])
-			hashes, _ := storage.SplitAndStore(bytes.NewReader(content), 256)
-			
 			for _, hash := range hashes {
 				data, _ := storage.GetBlock(hash)
 				p2p.ReplicateBlock(ctx, kadDHT, hash, data, 3)
 			}
-			fmt.Printf("Encrypted hashes: %v\n", hashes)
+
+		case "get":
+			hash := os.Args[2]
+			data, _ := p2p.GetFromDHT(ctx, kadDHT, hash)
+			fmt.Printf("Retrieved data: %s\n", string(data))
+
+		case "keys":
+			fmt.Printf("Public Key:\n%s\n", crypto.PublicKeyToBytes(storage.PublicKey()))
+
+		case "put-encrypted":
+			content, _ := os.ReadFile(os.Args[2])
+			hashes, _ := storage.SplitAndStore(bytes.NewReader(content), 256)
+			for _, hash := range hashes {
+				data, _ := storage.GetBlock(hash)
+				p2p.ReplicateBlock(ctx, kadDHT, hash, data, 3)
+			}
+
+		case "put-dag":
+			content, _ := os.ReadFile(os.Args[2])
+			hashes, _ := storage.SplitAndStore(bytes.NewReader(content), 256)
+			root, _ := storage.BuildDAG(hashes)
+			dagHash, _ := storage.StoreDAG(root)
+			p2p.ReplicateBlock(ctx, kadDHT, dagHash, nil, 3)
+			fmt.Printf("DAG root: %s\n", dagHash)
+
+		case "verify-dag":
+			rootData, _ := p2p.GetFromDHT(ctx, kadDHT, os.Args[2])
+			var root dag.Node
+			json.Unmarshal(rootData, &root)
+			if err := dag.Verify(&root); err != nil {
+				fmt.Printf("Verification failed: %v\n", err)
+			} else {
+				fmt.Println("DAG is valid")
+			}
+
+		default:
+			log.Fatal("Unknown command")
 		}
 	}
 
