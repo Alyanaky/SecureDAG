3aff57511e20abcafd195ccfdd7507ae35fda0e3
Alyanaky <007izosimov@gmail.com>
2025-05-27 10:14:12 +0500

    Update self_heal.go

--- internal/storage/self_heal.go
+++ internal/storage/self_heal.go
@@ -1,57 +1,24 @@
 package storage
 
 import (
-	"context"
-	"time"
+    "context"
+    "time"
 
-	"github.com/Alyanaky/SecureDAG/internal/p2p"
+    "github.com/dgraph-io/badger/v4"
 )
 
-const (
-	HealInterval = 1 * time.Hour
-	MinReplicas  = 3
-)
+func (s *BadgerStore) SelfHeal(ctx context.Context) error {
+    ticker := time.NewTicker(s.healInterval)
+    defer ticker.Stop()
 
-func (s *BadgerStore) StartSelfHeal(ctx context.Context, dht *dht.IpfsDHT) {
-	ticker := time.NewTicker(HealInterval)
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				s.checkAndHealBlocks(dht)
-			case <-ctx.Done():
-				return
-			}
-		}
-	}()
-}
-
-func (s *BadgerStore) checkAndHealBlocks(dht *dht.IpfsDHT) {
-	s.db.View(func(txn *badger.Txn) error {
-		it := txn.NewIterator(badger.DefaultIteratorOptions)
-		defer it.Close()
-
-		for it.Rewind(); it.Valid(); it.Next() {
-			item := it.Item()
-			key := item.Key()
-			if !bytes.HasPrefix(key, []byte("meta_")) {
-				continue
-			}
-
-			var meta BlockMetadata
-			item.Value(func(val []byte) error {
-				json.Unmarshal(val, &meta)
-				if meta.References < MinReplicas {
-					go s.healBlock(string(key[5:]), dht)
-				}
-				return nil
-			})
-		}
-		return nil
-	})
-}
-
-func (s *BadgerStore) healBlock(hash string, dht *dht.IpfsDHT) {
-	data, _ := s.GetBlock(hash)
-	p2p.ReplicateBlock(context.Background(), dht, hash, data, MinReplicas)
+    for {
+        select {
+        case <-ctx.Done():
+            return nil
+        case <-ticker.C:
+            if err := s.healBlock(ctx); err != nil {
+                return err
+            }
+        }
+    }
 }
