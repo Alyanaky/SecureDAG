c82c3328b5198ab2f3fdc3443c4b43dcfd588ab5
Alyanaky <007izosimov@gmail.com>
2025-05-27 09:41:48 +0500

    Update manager.go

--- internal/crypto/manager.go
+++ internal/crypto/manager.go
@@ -1,78 +1,79 @@
 package crypto
 
-import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha256"
-	"crypto/x509"
-	"encoding/pem"
-	"errors"
-	"io"
-)
+   import (
+       "crypto/aes"
+       "crypto/cipher"
+       "crypto/rand"
+       "crypto/rsa"
+       "crypto/sha256"
+       "errors"
+   )
 
-func GenerateRSAKeys() (*rsa.PrivateKey, *rsa.PublicKey, error) {
-	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return nil, nil, err
-	}
-	return privateKey, &privateKey.PublicKey, nil
-}
+   type KeyManager struct {
+       privateKey *rsa.PrivateKey
+       publicKey  *rsa.PublicKey
+   }
 
-func EncryptAESKey(publicKey *rsa.PublicKey, aesKey []byte) ([]byte, error) {
-	return rsa.EncryptOAEP(
-		sha256.New(),
-		rand.Reader,
-		publicKey,
-		aesKey,
-		nil,
-	)
-}
+   func NewKeyManager() *KeyManager {
+       privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+       if err != nil {
+           panic(err)
+       }
+       return &KeyManager{
+           privateKey: privateKey,
+           publicKey:  &privateKey.PublicKey,
+       }
+   }
 
-func DecryptAESKey(privateKey *rsa.PrivateKey, encryptedKey []byte) ([]byte, error) {
-	return rsa.DecryptOAEP(
-		sha256.New(),
-		rand.Reader,
-		privateKey,
-		encryptedKey,
-		nil,
-	)
-}
+   func (km *KeyManager) GetPublicKey() *rsa.PublicKey {
+       return km.publicKey
+   }
 
-func EncryptData(data, aesKey []byte) ([]byte, error) {
-	block, _ := aes.NewCipher(aesKey)
-	gcm, _ := cipher.NewGCM(block)
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return nil, err
-	}
-	return gcm.Seal(nonce, nonce, data, nil), nil
-}
+   func (km *KeyManager) GetPrivateKey() *rsa.PrivateKey {
+       return km.privateKey
+   }
 
-func DecryptData(cipherData, aesKey []byte) ([]byte, error) {
-	block, _ := aes.NewCipher(aesKey)
-	gcm, _ := cipher.NewGCM(block)
-	nonceSize := gcm.NonceSize()
-	if len(cipherData) < nonceSize {
-		return nil, errors.New("invalid cipher data")
-	}
-	
-	nonce, ciphertext := cipherData[:nonceSize], cipherData[nonceSize:]
-	return gcm.Open(nil, nonce, ciphertext, nil)
-}
+   func (km *KeyManager) SetKeys(privateKey *rsa.PrivateKey, publicKey *rsa.PublicKey) {
+       km.privateKey = privateKey
+       km.publicKey = publicKey
+   }
 
-func PublicKeyToBytes(pub *rsa.PublicKey) []byte {
-	return pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PUBLIC KEY",
-		Bytes: x509.MarshalPKCS1PublicKey(pub),
-	})
-}
+   func (km *KeyManager) EncryptData(data, aesKey []byte) ([]byte, error) {
+       block, err := aes.NewCipher(aesKey)
+       if err != nil {
+           return nil, err
+       }
+       gcm, err := cipher.NewGCM(block)
+       if err != nil {
+           return nil, err
+       }
+       nonce := make([]byte, gcm.NonceSize())
+       if _, err := rand.Read(nonce); err != nil {
+           return nil, err
+       }
+       return gcm.Seal(nonce, nonce, data, nil), nil
+   }
 
-func BytesToPublicKey(pubBytes []byte) (*rsa.PublicKey, error) {
-	block, _ := pem.Decode(pubBytes)
-	if block == nil {
-		return nil, errors.New("failed to parse PEM block")
-	}
-	return x509.ParsePKCS1PublicKey(block.Bytes)
-}
+   func (km *KeyManager) DecryptData(encrypted, aesKey []byte) ([]byte, error) {
+       block, err := aes.NewCipher(aesKey)
+       if err != nil {
+           return nil, err
+       }
+       gcm, err := cipher.NewGCM(block)
+       if err != nil {
+           return nil, err
+       }
+       if len(encrypted) < gcm.NonceSize() {
+           return nil, errors.New("encrypted data too short")
+       }
+       nonce, ciphertext := encrypted[:gcm.NonceSize()], encrypted[gcm.NonceSize():]
+       return gcm.Open(nil, nonce, ciphertext, nil)
+   }
+
+   func (km *KeyManager) EncryptAESKey(pubKey *rsa.PublicKey, aesKey []byte) ([]byte, error) {
+       return rsa.EncryptOAEP(sha256.New(), rand.Reader, pubKey, aesKey, nil)
+   }
+
+   func (km *KeyManager) DecryptAESKey(privKey *rsa.PrivateKey, encryptedKey []byte) ([]byte, error) {
+       return rsa.DecryptOAEP(sha256.New(), rand.Reader, privKey, encryptedKey, nil)
+   }
