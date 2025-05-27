4c2f9a936ac5687091419d0c80f13cf027fb0e1b
Alyanaky <007izosimov@gmail.com>
2025-05-27 10:04:37 +0500

    Update key_rotation.go

--- internal/crypto/key_rotation.go
+++ internal/crypto/key_rotation.go
@@ -3,7 +3,6 @@ package crypto
 import (
     "crypto/rand"
     "crypto/rsa"
-    "sync"
     "time"
 )
 
@@ -12,15 +11,12 @@ func RotateKeys(km *KeyManager, interval time.Duration) error {
     defer ticker.Stop()
 
     for range ticker.C {
-        km.mu.Lock()
         newPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
         if err != nil {
-            km.mu.Unlock()
             return err
         }
         newPubKey := &newPrivKey.PublicKey
         km.SetKeys(newPrivKey, newPubKey)
-        km.mu.Unlock()
     }
     return nil
 }
