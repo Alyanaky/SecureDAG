8597d0303f7800c856c18dbfb4954422f049a923
Alyanaky <007izosimov@gmail.com>
2025-05-27 10:14:38 +0500

    Update dag.go

--- internal/storage/dag.go
+++ internal/storage/dag.go
@@ -1,49 +1,16 @@
 package storage
 
 import (
-	"bytes"
-	"encoding/json"
-	
-	"github.com/Alyanaky/SecureDAG/internal/dag"
+    "context"
+    "crypto/sha256"
+
+    "github.com/Alyanaky/SecureDAG/internal/dag"
+    "github.com/dgraph-io/badger/v4"
 )
 
-func (s *BadgerStore) BuildDAG(hashes []string) (*dag.Node, error) {
-	nodes := make([]*dag.Node, len(hashes))
-	
-	for i, hash := range hashes {
-		data, err := s.GetBlock(hash)
-		if err != nil {
-			return nil, err
-		}
-		nodes[i] = dag.NewMerkleNode(nil, data)
-	}
-
-	root := buildMerkleTree(nodes)
-	return root, nil
-}
-
-func buildMerkleTree(nodes []*dag.Node) *dag.Node {
-	if len(nodes) == 1 {
-		return nodes[0]
-	}
-
-	var parents []*dag.Node
-	for i := 0; i < len(nodes); i += 2 {
-		if i+1 >= len(nodes) {
-			parents = append(parents, nodes[i])
-			continue
-		}
-		parent := dag.NewMerkleNode([]*dag.Node{nodes[i], nodes[i+1]}, nil)
-		parents = append(parents, parent)
-	}
-	return buildMerkleTree(parents)
-}
-
-func (s *BadgerStore) StoreDAG(root *dag.Node) (string, error) {
-	buf := new(bytes.Buffer)
-	enc := json.NewEncoder(buf)
-	if err := enc.Encode(root); err != nil {
-		return "", err
-	}
-	return s.PutBlock(buf.Bytes())
+func (s *BadgerStore) StoreDAG(ctx context.Context, node *dag.MerkleNode) error {
+    hash := sha256.Sum256(node.Hash)
+    return s.db.Update(func(txn *badger.Txn) error {
+        return txn.Set(hash[:], node.Marshal())
+    })
 }
