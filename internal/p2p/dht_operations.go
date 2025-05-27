266ab7c244a28249a9e22a822bb5f9cfedd01d22
Alyanaky <007izosimov@gmail.com>
2025-05-27 09:59:54 +0500

    Update dht_operations.go

--- internal/p2p/dht_operations.go
+++ internal/p2p/dht_operations.go
@@ -4,7 +4,8 @@ import (
     "context"
     "log"
     dht "github.com/libp2p/go-libp2p-kad-dht"
-    "github.com/libp2p/go-libp2p/core/peer"
+    "github.com/ipfs/go-cid"
+    "github.com/multiformats/go-multihash"
 )
 
 type DHTOperations struct {
@@ -21,7 +22,14 @@ func (ops *DHTOperations) ReplicateData(ctx context.Context, key string, data []
         return err
     }
 
-    providers, err := ops.dht.FindProviders(ctx, key)
+    // Convert string key to CID
+    mh, err := multihash.Sum([]byte(key), multihash.SHA2_256, -1)
+    if err != nil {
+        return err
+    }
+    cidKey := cid.NewCidV1(cid.Raw, mh)
+
+    providers, err := ops.dht.FindProviders(ctx, cidKey)
     if err != nil {
         return err
     }
