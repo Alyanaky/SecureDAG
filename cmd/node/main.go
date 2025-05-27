45a12e0adc41a0b58c88b53de7bec2ee3e0aaad1
Alyanaky <007izosimov@gmail.com>
2025-05-27 10:50:44 +0500

    Update main.go

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,48 +1,25 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-	"time"
+    "context"
+    "log"
 
-	"github.com/Alyanaky/SecureDAG/internal/p2p"
-	"github.com/Alyanaky/SecureDAG/internal/storage"
+    "github.com/Alyanaky/SecureDAG/internal/storage"
 )
 
 func main() {
-	ctx := context.Background()
-	
-	storePath := filepath.Join(".", "storage", fmt.Sprintf("node-%d", time.Now().UnixNano()))
-	store, err := storage.NewBadgerStore(storePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer store.Close()
+    store, err := storage.NewBadgerStore("/tmp/securedag")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer store.Close()
 
-	kadDHT, node, err := p2p.NewDHT(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer kadDHT.Close()
+    ctx := context.Background()
+    go func() {
+        if err := store.SelfHeal(ctx); err != nil {
+            log.Printf("Self-healing failed: %v", err)
+        }
+    }()
 
-	store.StartSelfHeal(ctx, kadDHT)
-
-	node.SetStreamHandler("/secure-dag/1.0", func(s network.Stream) {
-		defer s.Close()
-		buf := make([]byte, 1024)
-		n, _ := s.Read(buf)
-		msg := string(buf[:n])
-		
-		switch {
-		case strings.HasPrefix(msg, "GET:"):
-			handleGetRequest(s, msg, store)
-		case strings.HasPrefix(msg, "DAG:"):
-			handleDAGRequest(s, msg, store)
-		}
-	})
-
-	select {}
+    select {}
 }
