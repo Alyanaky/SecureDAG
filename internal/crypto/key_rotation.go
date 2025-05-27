c0a12adb149b12140d090281536fde68aab00b10
Alyanaky <007izosimov@gmail.com>
2025-05-27 09:48:05 +0500

    Update key_rotation.go

--- internal/crypto/key_rotation.go
+++ internal/crypto/key_rotation.go
@@ -1,45 +1,32 @@
 package crypto
 
 import (
-	"context"
-	"crypto/rand"
-	"crypto/rsa"
-	"time"
+    "crypto/rand"
+    "crypto/rsa"
+    "sync"
+    "time"
 )
 
-const (
-	KeyRotationInterval = 30 * 24 * time.Hour
-)
-
-func (m *KeyManager) StartRotation(ctx context.Context, reencryptFunc func(oldPriv *rsa.PrivateKey, newPub *rsa.PublicKey) error) {
-	ticker := time.NewTicker(KeyRotationInterval)
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				if err := m.RotateKeys(reencryptFunc); err != nil {
-					// log error
-				}
-			case <-ctx.Done():
-				return
-			}
-		}
-	}()
+type KeyManager struct {
+    privateKey *rsa.PrivateKey
+    publicKey  *rsa.PublicKey
+    mu         sync.Mutex
 }
 
-func (m *KeyManager) RotateKeys(reencryptFunc func(oldPriv *rsa.PrivateKey, newPub *rsa.PublicKey) error) error {
-	newPriv, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return err
-	}
-	newPub := &newPriv.PublicKey
-	m.mu.Lock()
-	oldPriv := m.privateKey
-	m.privateKey = newPriv
-	m.publicKey = newPub
-	m.mu.Unlock()
-	if err := reencryptFunc(oldPriv, newPub); err != nil {
-		return err
-	}
-	return nil
+func RotateKeys(km *KeyManager, interval time.Duration) error {
+    ticker := time.NewTicker(interval)
+    defer ticker.Stop()
+
+    for range ticker.C {
+        km.mu.Lock()
+        newPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+        if err != nil {
+            km.mu.Unlock()
+            return err
+        }
+        newPubKey := &newPrivKey.PublicKey
+        km.SetKeys(newPrivKey, newPubKey)
+        km.mu.Unlock()
+    }
+    return nil
 }
